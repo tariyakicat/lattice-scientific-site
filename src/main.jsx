eb2431b2c40a7c379615b1c87d6ccba828dbb5b8
Yinan Yang <yangyinan8097@gmail.com>
2026-06-18 00:06:12 +0100

    Add analytics hook and clean redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { initAnalytics } from "./analytics.js";
 import App from "./App.jsx";
 import "./index.css";
+
+initAnalytics();
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
